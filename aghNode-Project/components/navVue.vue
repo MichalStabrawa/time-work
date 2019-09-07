8270ec93bbfc0d97d1b9d4805234280a4c431bc3
Michal Stabrawa <mistaaqua@gmail.com>
2019-09-07 21:40:40 +0200

    rwd menu

--- aghNode-Project/components/navVue.vue
+++ aghNode-Project/components/navVue.vue
@@ -1,11 +1,12 @@
 <template>
   <nav class="nav">
-   <button class="hamburger"><i class="fas fa-bars"></i></button> 
-   <div class="menu-item">
-<nuxt-link class="link" to="/">Home</nuxt-link>
-    <nuxt-link class="link" to="/select">Pracownicy</nuxt-link>
-   </div>
-    
+    <button class="hamburger" @click="showMenu">
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="menu-item" v-bind:class="{ active: isActive }">
+      <nuxt-link class="link" to="/">Home</nuxt-link>
+      <nuxt-link class="link" to="/select">Pracownicy</nuxt-link>
+    </div>
   </nav>
 </template>
 
@@ -13,12 +14,16 @@
 export default {
   data() {
     return {
-      flag: false
+      flag: false,
+      isActive: false
     };
   },
   methods: {
     active() {
       console.log("Test");
+    },
+    showMenu() {
+      this.isActive = !this.isActive;
     }
   }
 };
@@ -27,14 +32,28 @@ export default {
 <style lang="scss" scoped>
 .nav {
   height: 80px;
-  background-image: linear-gradient(to right, #dad9d4, #e0ddd9, #e5e1de, #e9e5e4, #ede9e9, #ede3e5, #eadce3, #e5d7e4, #d7c6df, #c3b7dc, #a9aad9, #889ed7);;
+  background-image: linear-gradient(
+    to right,
+    #dad9d4,
+    #e0ddd9,
+    #e5e1de,
+    #e9e5e4,
+    #ede9e9,
+    #ede3e5,
+    #eadce3,
+    #e5d7e4,
+    #d7c6df,
+    #c3b7dc,
+    #a9aad9,
+    #889ed7
+  );
   padding-left: 50px;
   padding-right: 50px;
   display: flex;
   align-items: center;
 
   a {
-    color: #6A71A6;
+    color: #6a71a6;
     text-decoration: none;
     font-size: 20px;
     font-weight: bold;
@@ -42,10 +61,10 @@ export default {
     padding: 10px;
 
     &:hover {
-      color: #181C24;
+      color: #181c24;
     }
-    &:focus{
-      color: #181C24;
+    &:focus {
+      color: #181c24;
     }
   }
   .hamburger {
@@ -53,13 +72,11 @@ export default {
     height: 30px;
     margin-top: 0px;
     background: transparent;
-    cursor:pointer;
+    cursor: pointer;
     display: none;
-
-
   }
 }
-@media(max-width:768px) {
+@media (max-width: 768px) {
   .nav {
     position: relative;
     flex-direction: column;
@@ -67,24 +84,30 @@ export default {
     justify-content: center;
     .hamburger {
       display: block;
+      cursor:pointer;
     }
     .menu-item {
-   position: absolute;
-   z-index: 1000;
-   top: 80px;
-   display: flex;
-   flex-direction: column;
-   background:#000;
-   width: 100%;
-   margin-left: -50px;
-   padding-left: 40px;
-   width: 100%;
-   
+      position: absolute;
+      z-index: 100;
+      top: 80px;
+      display: flex;
+      flex-direction: column;
+      background: #E4E9EF;
+      width: 100%;
+      margin-left: -50px;
+      padding-left: 40px;
+      width: 100%;
+      display: none;
+      
+      &.active {
+        display: flex;
+      }
+a{
+  cursor: pointer;
+}
     }
   }
 }
-
-
 </style>
 
 
